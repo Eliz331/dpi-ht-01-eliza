fd99900af1fac7b9e5b9639a2ac69dbbe71530c2
Eliz331 <elie3112ae@gmail.com>
2026-09-23 02:24:32 +0300

    Add GET route to return submission data

---
+++ app/submission.json/route.js
@@ -0,0 +1,4 @@
+import submission from "../../../submission.json";
+export async function GET() {
+  return Response.json(submission, { headers: { "Cache-Control": "public, max-age=0, must-revalidate" } });
+}
