25306d80f095b55fa81e9a9e11db5606bdf0c1a7
Eliz331 <elie3112ae@gmail.com>
2026-09-23 02:04:26 +0200

    Fix import path for submission.json

--- app/submission.json/route.js
+++ app/submission.json/route.js
@@ -1,4 +1,4 @@
-import submission from "../../../submission.json";
+import submission from "../../submission.json";
 export async function GET() {
   return Response.json(submission, { headers: { "Cache-Control": "public, max-age=0, must-revalidate" } });
 }
